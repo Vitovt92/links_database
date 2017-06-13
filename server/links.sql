95582f4290adf50d9f912187082bbee5260683ca
links <Vitovt92@rambler.ru>
2017-06-13 17:05:36 +0300

    New tables added to SQL database

--- server/links.sql
+++ server/links.sql
@@ -24,3 +24,60 @@ CHARACTER SET = utf8;
 
 INSERT INTO street_table (name_street_new, comment)
 VALUES('Предславинская', 'Метро "Палац Украина"');
+
+CREATE TABLE place_table (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  bilding_table_id int REFERENCES bilding_table(id),
+  description_of_place varchar(1000),
+  comment varchar(3000),
+  foto_url varchar(1000)
+)
+CHARACTER SET = utf8;
+
+CREATE TABLE switches_table (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  place_table_id int REFERENCES place_table(id),
+  alias varchar(100),
+  ip_of_switch varchar(100),
+  comment varchar(3000),
+  foto_url varchar(1000),
+  namber_of_ports int
+)
+CHARACTER SET = utf8;
+
+CREATE TABLE ports_of_switch_table (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  dates TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  switches_table_id int REFERENCES switches_table(id),
+  namber_of_port int,
+  comment varchar(3000)
+)
+CHARACTER SET = utf8;
+
+CREATE TABLE type_of_connection_table (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  type_of_connection varchar(1000),
+  comment varchar(3000)
+
+)
+CHARACTER SET = utf8;
+
+CREATE TABLE connection_table (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  dates TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  place_table_id_1 int REFERENCES place_table(id),
+  place_table_id_2 int REFERENCES place_table(id),
+  type_of_connection_table_id int REFERENCES type_of_connection_table(id),
+  abonent_teble_id int REFERENCES abonent_teble(id),
+  comment varchar(3000)
+)
+CHARACTER SET = utf8;
+
+CREATE TABLE abonent_table (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  dates TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  ports_of_switch_table_id int REFERENCES ports_of_switch_table(id),
+  place_table_id int REFERENCES place_table(id),
+  comment varchar(3000)
+)
+CHARACTER SET = utf8;
